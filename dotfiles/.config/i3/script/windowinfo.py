1a319f67746d463bfb7ce9b08ab1ef40c6ae95b3
Brian Hsu <brianhsu.hsu@gmail.com>
2017-06-17 19:30:47 +0800

    Improve drop-down terminal.

--- dotfiles/.config/i3/script/windowinfo.py
+++ dotfiles/.config/i3/script/windowinfo.py
@@ -19,8 +19,14 @@ curmon = screen.get_monitor_at_window(screen.get_active_window())
 x, y, width, height = monitors[curmon]
 
 command = sys.argv[1]
+widthSub = 0
+heightSub = 0
+
+if len(sys.argv) == 4:
+  widthSub = int(sys.argv[2]);
+  heightSub = int(sys.argv[3]);
 
 if command == 'size':
-    print "%d %d" % (width-10,height-90)
+    print "%d %d" % (width - 10 - widthSub, height - 90 - heightSub )
 elif command == 'position':
     print "%d 56" % (x+3)
